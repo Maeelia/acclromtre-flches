e663e027f1532ca6cc3b1f5d3aabeab64ee2cd48
Maeelia <103173774+Maeelia@users.noreply.github.com>
2022-04-21 09:22:57 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,37 @@
+basic.forever(function () {
+    if (input.acceleration(Dimension.Y) < -200) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.Y) > 200) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) > 200) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else if (input.acceleration(Dimension.X) < -200) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Yes)
+    }
+})
